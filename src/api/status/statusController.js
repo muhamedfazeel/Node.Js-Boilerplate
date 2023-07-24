739c82454f0882faff93660f51da995e58115af4
Fazeel <devfazeel642@gmail.com>
2023-07-24 16:39:31 +0530

    Move statusController to controller directory

--- src/api/status/statusController.js
+++
@@ -1,7 +0,0 @@
-const { HTTP_OK } = require('../../utils/httpStatusCodes');
-const { getStatusService } = require('./statusService');
-
-exports.getStatusController = async (req, res, next) => {
-    const status = getStatusService();
-    res.status(HTTP_OK).json(status);
-};
