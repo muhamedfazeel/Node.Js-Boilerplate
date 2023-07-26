90f153311f70f0f2036c5865da02150c190a9595
Fazeel <devfazeel642@gmail.com>
2023-07-26 16:37:42 +0530

    Move response messages to a new file

---
+++ src/utils/responseMessages.js
@@ -0,0 +1,5 @@
+module.exports.INTERNAL_SERVER_ERROR_MSG = 'Internal Server Error';
+module.exports.DEFAULT_ERR_MSG = 'An Error occured: ';
+module.exports.UNEXPECTED_ERR_MSG = 'Unexpected Error';
+module.exports.INVALID_USERNAME_PASSWORD_ERR_MSG = 'Invalid username/password';
+module.exports.USER_CREATED_MESSAGE = 'User created successfully';
