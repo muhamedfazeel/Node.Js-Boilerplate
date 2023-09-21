80115ebdfcbf8b876b6dfae1c3cb1db6fd55cc03
Fazeel <devfazeel642@gmail.com>
2023-09-21 11:35:30 +0530

    Update logger configuration

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,7 +3,24 @@ const config = require('../config/config');
 
 const logFormat = format.combine(
     format.colorize(),
-    format.label({ label: config.PROJECT }),
+    format.label({ label: config.project }),
+    format.timestamp({ format: 'MM-DD-YYYY HH:mm:ss' }),
+    format.printf(({ level, message, label, timestamp, ...info }) => {
+        let log = `${timestamp} [${label}] [${level}]`;
+
+        if (Object.keys(info).length > 0) {
+            log += '\nAdditional Info: ' + JSON.stringify(info, null, 2);
+        }
+
+        log += '\nMessage: ' + message + '\n';
+
+        return log;
+    })
+);
+
+const errorLogFormat = format.combine(
+    format.colorize(),
+    format.label({ label: config.project }),
     format.timestamp({ format: 'MM-DD-YYYY HH:mm:ss' }),
     format.printf(({ level, message, label, timestamp, ...info }) => {
         let log = `${timestamp} [${label}] [${level}]`;
@@ -35,9 +52,11 @@ module.exports.logger = createLogger({
         new transports.File({
             level: 'error',
             filename: 'logs/error.log',
+            format: errorLogFormat
         }),
         new transports.File({
             filename: 'logs/logs.log', // Log all levels to log.log
         }),
     ],
+    exitOnError: false
 });
