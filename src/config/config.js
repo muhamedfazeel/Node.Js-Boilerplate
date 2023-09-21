b196f71180401f6fbd94649374787843bbea356e
Fazeel <devfazeel642@gmail.com>
2023-09-21 11:34:53 +0530

    update config file to read credentials from env

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,28 @@
 require('dotenv').config();
+const {
+  PROJECT,
+  PORT,
+  SERVER_ENV,
+  DB_USER,
+  DB_HOST,
+  DB_NAME,
+  DB_PASS,
+  DB_PORT,
+  SECRET,
+  SALT_ROUNDS
+} = process.env;
 
-exports.config = process.env;
+const config = {
+  project: PROJECT,
+  port: PORT,
+  serverEnv: SERVER_ENV,
+  dbUser: DB_USER,
+  dbHost: DB_HOST,
+  dbName: DB_NAME,
+  dbPass: DB_PASS,
+  dbPort: DB_PORT,
+  secret: SECRET,
+  saltRounds: SALT_ROUNDS
+};
+
+module.exports = config;
