8c74d494e9ca62916195c866a4c90c04a57dd416
Fazeel <fazeel642@gmail.com>
2023-12-26 17:30:22 +0530

    Reorder DEFUALT_ERR_MSG require statement

--- src/utils/dbClient.js
+++ src/utils/dbClient.js
@@ -1,7 +1,7 @@
 const { pool } = require('../config/database');
-const { DEFAULT_ERR_MSG } = require('./constants');
 const { AppError } = require('./errorHandler');
 const { logger } = require('./logger');
+const { DEFAULT_ERR_MSG } = require('./messages');
 
 module.exports.runQuery = async (query, params) => {
   const client = await pool.connect();
